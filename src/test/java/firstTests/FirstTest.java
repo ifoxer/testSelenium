1cafb2d12d98d324ea7a576c2491b5ecf5995c99
ifoxer <romanliseykin@gmail.com>
2020-02-17 00:15:35 +0300

    Initial commit

---
+++ src/test/java/firstTests/FirstTest.java
@@ -0,0 +1,27 @@
+package firstTests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    public ChromeDriver driver;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "/Users/liseykin/Drivers/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://yandex.ru/");
+    }
+
+    @After
+     public void  close() {
+            driver.quit();
+    }
+}
